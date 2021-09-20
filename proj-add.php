875d4ea2e772565755b04a87d92b7caa62a91dba
samy <samuca.oliveira2003@gmail.com>
2021-09-20 15:21:39 -0300

    upload de img

--- proj-add.php
+++ proj-add.php
@@ -16,44 +16,50 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 
-<body >
+<body>
   <?php
-      require_once "includes/connection-bd.php";
-      require_once "includes/function/function-login.php";
-      require_once "includes/function/function-password.php";
-      require_once "includes/function/function-msg.php";
-      require_once "includes/function/function-img.php";
-    ?>
+  require_once "includes/connection-bd.php";
+  require_once "includes/function/function-login.php";
+  require_once "includes/function/function-password.php";
+  require_once "includes/function/function-msg.php";
+  require_once "includes/function/function-img.php";
+  ?>
   <?php
-    require_once "includes/header.php";
-    ?>
+  require_once "includes/header.php";
+  ?>
   <?php
-        if(is_logado()){
-          if(!isset($_POST['nomeProj'])){
-            require_once "includes/forms/proj-add-form.html";
-        }else{
-          echo"ok";
-          $user = $_SESSION['id'];
-          $data =  date("y-m-d h:m:s");
-          $nome = $_POST['nomeProj']??null;
-          $desc = $_POST['desc']??null;
-          $tag = $_POST['tag']??null;
-          $bg_projet = $_FILES['bg_projet']['name']??null;
-          $array =$_FILES['files']['name']??null;
-          if(empty($nome)||empty($desc)||empty($tag)||empty($bg_projet)||empty($array)){
-            echo msg_erro("Opps...","Todos os dados são obrigatórios.","Tentar Novamente","proj-add.php");
-          }else{
+  if (is_logado()) {
+    if (!isset($_POST['nomeProj'])) {
+      require_once "includes/forms/proj-add-form.html";
+    } else {
+      echo "ok";
+      $user = $_SESSION['id'];
+      $data =  date("y-m-d h:m:s");
+      $nome = $_POST['nomeProj'] ?? null;
+      $desc = $_POST['desc'] ?? null;
+      $tag = $_POST['tag'] ?? null;
+      $bg_projet = $_FILES['bg_projet']['name'] ?? null;
+      $array = $_FILES['files']['name'] ?? null;
+      if (empty($nome) || empty($desc) || empty($tag) || empty($bg_projet) || empty($array)) {
+        echo msg_erro("Opps...", "Todos os dados são obrigatórios.", "Tentar Novamente", "proj-add.php");
+      } else {
+        if (isset($_FILES['bg_projet'])) {
+          $ext = pathinfo($_FILES['bg_projet']['name'], PATHINFO_EXTENSION); //Pegando extensão do arquivo
+          $new_name = date("Y.m.d-H.i.s.") . $ext; //Definindo um novo nome para o arquivo
+          $dir = 'upload/image/'; //Diretório para uploads
+          move_uploaded_file($_FILES['bg_projet']['tmp_name'], $dir . $new_name); //Fazer upload do arquivo
+          $bg_projet = $new_name;
+          for($x = 0; $x < count($array); $x++){
             print_r(array_values ($array));
-            print_r($data);
-            print_r($user);
           }
         }
-        }
-        else{
-          echo msg_erro("Ops...","Você não pode acessar essa página.<br> Por favor efetue seu <u><b><a class='link-dark' href='user-login.php'>login</a></u></b> ou <u><b><a class='link-dark' href='user-cadastro.php'>cadastre-se</a></u></b> para publicar novas projetos . ","Voltar a explorar","index.php");
-        }
-        require "includes/footer.php"
-      ?>
+      }
+    }
+  } else {
+    echo msg_erro("Ops...", "Você não pode acessar essa página.<br> Por favor efetue seu <u><b><a class='link-dark' href='user-login.php'>login</a></u></b> ou <u><b><a class='link-dark' href='user-cadastro.php'>cadastre-se</a></u></b> para publicar novas projetos . ", "Voltar a explorar", "index.php");
+  }
+  require "includes/footer.php"
+  ?>
 
   <!--Bootstrap JS -->
   <script src="bootstrap/js/bootstrap.bundle.js"> </script>
--- user-login.php
+++ user-login.php
@@ -18,46 +18,44 @@
 
 <body class="yas_pag_reg yas_font_regular">
   <?php
-      require_once "includes/connection-bd.php";
-      require_once "includes/function/function-login.php";
-      require_once "includes/function/function-password.php";
-      require_once "includes/function/function-svg.php";
-      require_once "includes/function/function-msg.php";
-    ?>
+  require_once "includes/connection-bd.php";
+  require_once "includes/function/function-login.php";
+  require_once "includes/function/function-password.php";
+  require_once "includes/function/function-svg.php";
+  require_once "includes/function/function-msg.php";
+  ?>
   <?php
 
-      if(is_logado()){
-        echo msg_erro("Já tem alguem ai","Você ja está logado, em uma conta existente,para criar uma nova conta porfavor saia de sua conta ","voltar a explorar","index.php");
-      }
-      else{
-        if(!isset($_POST['email'])){
-          require "includes/forms/user-login-form.html";
-      }else{
-          $email=$_POST['email'];
-          $senha=$_POST['senha'];
-          $sql= "SELECT user_id, user_email, user_senha from user_yas where user_email='$email' limit 1";
-          $busca = $banco->query("$sql");
-          if(!$busca){
-            echo msg_erro('Opp..','Falha na busca do banco de dados, por favor tente denovo','Tentar Novamente','user-login.php');
+  if (is_logado()) {
+    echo msg_erro("Já tem alguem ai", "Você ja está logado, em uma conta existente,para criar uma nova conta porfavor saia de sua conta ", "voltar a explorar", "index.php");
+  } else {
+    if (!isset($_POST['email'])) {
+      require "includes/forms/user-login-form.html";
+    } else {
+      $email = $_POST['email'];
+      $senha = $_POST['senha'];
+      $sql = "SELECT user_id, user_email, user_senha from user_yas where user_email='$email' limit 1";
+      $busca = $banco->query("$sql");
+      if (!$busca) {
+        echo msg_erro('Opp..', 'Falha na busca do banco de dados, por favor tente denovo', 'Tentar Novamente', 'user-login.php');
+      } else {
+        if ($busca->num_rows > 0) {
+          $reg = $busca->fetch_object();
+          // print_r($reg);
+          if (testar_hash($senha, $reg->user_senha)) {
+            $_SESSION['email'] = $reg->user_email;
+            $_SESSION['id'] = $reg->user_id;
+            header("Location: index.php");
+          } else {
+            echo msg_erro("Senha Inválida", "Por favor, insira a senha corretamente. ", "Tentar Novamente", "user-login.php");
           }
-          else{
-            if($busca->num_rows>0){
-              $reg = $busca->fetch_object();
-              // print_r($reg);
-              if(testar_hash($senha, $reg->user_senha)){
-                $_SESSION['email'] = $reg->user_email;
-                $_SESSION['id'] = $reg->user_id;
-                header("Location: index.php");
-              }else{
-                echo msg_erro("Senha Inválida","Por favor, insira a senha corretamente. ","Tentar Novamente","user-login.php");
-              }
-            }else{
-              echo msg_erro("Opps..","Você ainda não existe, tente novamente ou <br><a href='user_cadastro.php' class='link-dark'><u><b>CADASTRE-SE</b></u></a>.","Tentar Novamente","user-login.php");
-            }
-          }
+        } else {
+          echo msg_erro("Opps..", "Você ainda não existe, tente novamente ou <br><a href='user_cadastro.php' class='link-dark'><u><b>CADASTRE-SE</b></u></a>.", "Tentar Novamente", "user-login.php");
         }
       }
-    ?>
+    }
+  }
+  ?>
   <!--Bootstrap JS -->
   <script src="bootstrap/js/bootstrap.bundle.js"> </script>
 
