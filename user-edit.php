4162cbf969040d7566e27ed463cb3be0c0f44cbe
Thais Oliveira <oliveirath27@gmail.com>
2021-09-28 16:44:05 -0300

    upload de editar perfil

--- user-edit.php
+++ user-edit.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 
-<body style="background-color:#e6e0de;">
+<body style="background-color:#204b3d;">
     <?php
       require_once "includes/connection-bd.php";
       require_once "includes/function/function-login.php";
@@ -26,16 +26,37 @@
     ?>
     <?php
         require_once "includes/header.php";
-        $cod = $_GET['cod'] ?? null;
+        $cod = $_SESSION['id'] ?? null;
         if(is_logado()){
-            require_once "includes/forms/user-edit-form.php";
+            if(!isset($_POST['nome'])){
+                require_once "includes/forms/user-edit-form.php";
+            }else{
+                $nome = $_POST['nome'] ?? null;
+                $sobrenome = $_POST['sobrenome'] ?? null;
+                $ocupacao = $_POST['ocupacao'] ?? null;
+                $desc = $_POST['descricao'] ?? null;
+                $email = $_POST['cont-email'] ?? null;
+                $telefone = $_POST['phone'] ?? null;
+                $whatsapp = $_POST['whatsapp'] ?? null;
+                $insta = $_POST['insta'] ?? null;
+                $twitter = $_POST['twitter'] ?? null;
+                $link = $_POST['link'] ?? null;
+                
+                $q="update user_yas set user_first_name='$nome', user_last_name='$sobrenome', user_carreira='$ocupacao', user_desc='$desc',
+                user_email_cont='$email', user_telefone_cont='$telefone', user_whatsapp_cont='$whatsapp', social_insta='$insta', 
+                social_twitter='$twitter', social_url='$link' where user_id = '".$_SESSION['id']."'";
+
+                if($banco->query($q)){
+                    echo msg_sucesso("Parabéns...","$nome $sobrenome, seus dados foram alterados com sucesso.","Voltar a Explorar","index.php");
+                }
+            }
+
         }
         else{
             echo msg_erro("Ops...","Você não pode acessar essa página.<br> Por favor efetue seu <u><b><a class='link-dark' href='user-login.php'>login</a></u></b> ou <u><b><a class='link-dark' href='user-cadastro.php'>cadastre-se</a></u></b>. ","Voltar a explorar","index.php");
         }
         require "includes/footer.php"
       ?>
-
 
     <!--Bootstrap JS -->
     <script src="bootstrap/js/bootstrap.bundle.js"> </script>
