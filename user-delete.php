09d47ef26648e1703193d4af4078797ed04a0e80
Thais Oliveira <oliveirath27@gmail.com>
2021-10-06 14:50:11 -0300

    pag delete

---
+++ user-delete.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+  <head>
+    <title>Your Save Art</title>
+    <!-- Meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- favincon -->
+      <link rel="shortcut icon" href="layout/icon/favicon.ico" />
+    <!-- Arquivo CSS -->
+      <link type="text/css" rel="stylesheet" href="layout/css/style.css">
+    <!-- Bootstrap CSS -->
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  </head>
+
+  <body>
+    <?php
+      require_once "includes/connection-bd.php";
+      require_once "includes/function/function-login.php";
+      require_once "includes/function/function-password.php";
+      require_once "includes/function/function-msg.php";
+      require_once "includes/function/function-img.php";
+    ?>
+
+
+    <!--Bootstrap JS -->
+    <script src="bootstrap/js/bootstrap.bundle.js"> </script>
+  </body>
+
+</html>
