d3cd6e374a881b0b9dfa938a85f5d8cde61e35b1
samy <samuca.oliveira2003@gmail.com>
2021-11-08 14:52:30 -0300

    check box img

--- includes/forms/proj-edit-form.php
+++ includes/forms/proj-edit-form.php
@@ -1,8 +1,21 @@
-<?php 
+<?php
 $busca = $banco->query("select * from projeto where proj_id='$cod'");
 $value = $busca->fetch_object();
 ?>
-
+<style>
+    .radio-image label > input{
+    visibility: hidden;
+}
+.radio-image label > input + img{
+    cursor:pointer;
+    border:4px solid #EEE;
+    border-radius:15px;
+    padding:10px;
+}
+.radio-image label > input:checked + img{
+    border:4px solid #3F51B5;
+}
+</style>
 <form action="proj-add.php" method="POST" enctype="multipart/form-data">
     <div class="container mt-5">
         <div class="row">
@@ -16,38 +29,53 @@ $value = $busca->fetch_object();
                 </label>
             </div>
             <div class="col-7 ps-5">
-                <label for="nomeProj">Nome do Projeto</label><input class="form-control shadow " type="text"
-                    name="nomeProj" id="nomeProj" size="30" maxlength="30" value=" <?php echo $value->proj_name?>">
-                <label for="desc">Descrição</label><textarea class="form-control shadow " id="desc"
-                    style="height: 100px" name="desc"><?php echo $value->proj_desc ?></textarea>
-                <label for="tag" name="tag" id="tag" >Tag</label>
+                <label for="nomeProj">Nome do Projeto</label><input class="form-control shadow " type="text" name="nomeProj" id="nomeProj" size="30" maxlength="30" value=" <?php echo $value->proj_name ?>">
+                <label for="desc">Descrição</label><textarea class="form-control shadow " id="desc" style="height: 100px" name="desc"><?php echo $value->proj_desc ?></textarea>
+                <label for="tag" name="tag" id="tag">Tag</label>
                 <select class="form-select shadow " name="tag" aria-label="Default select example">
                     <?php
-                    $q="select tag_id, tag_name from tag_proj";
+                    $q = "select tag_id, tag_name from tag_proj";
                     $busca = $banco->query($q);
-                    while($reg=$busca->fetch_object()){
-                        if($value->tag_id == $reg->tag_id ){
-                            $selected ='selected';
-                         }else{
-                            $selected ="";
+                    while ($reg = $busca->fetch_object()) {
+                        if ($value->tag_id == $reg->tag_id) {
+                            $selected = 'selected';
+                        } else {
+                            $selected = "";
                         }
                         echo "<option value='$reg->tag_id' $selected>$reg->tag_name</option>";
                     }
-                ?>
+                    ?>
                 </select>
             </div>
         </div>
     </div>
-    <div class="container mt-5 ps-5">
+    <div class="container mt-5 radio-image">
         <div class="row">
-            <div class="col-sm-2 imgUp">
-                <div class="imagePreview"></div><label class="btn btn-success"><i class="bi bi-cloud-upload"></i>
-                    Upload<input type="file" class="uploadFile img" name="files[]" multiple="multiple"
-                        value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label> <button
-                    type="button" class="btn btn-primary del"><i class="bi bi-trash "> </i> Deletar</button>
+            <div class="col-4">
+                <label for="M">
+                    <input type="checkbox" name="sexo" id="M" value="M">
+                    <img src="layout\image\bg-yas.png" alt="Masculino" height='300' width="480">
+                </label>
             </div>
-            <i class="bi bi-plus imgAdd p-0"></i>
+
+            <div class="col-4">
+                <label for="F">
+                    <input type="checkbox" name="sexo" id="F" value="F">
+                    <img src="img/female_black.png" alt="Feminino">
+                </label>
+            </div>
+            <div class="col-4">
+                <label for="F">
+                    <input type="checkbox" name="sexo" id="F" value="F">
+                    <img src="img/female_black.png" alt="Feminino">
+                </label>
+            </div>
+
         </div>
+
+    </div>
+
+    </div>
     </div>
     <div class="container mt-5 text-end">
         <button type="reset" class="btn btn-primary shadow me-3">Cancelar</button>
