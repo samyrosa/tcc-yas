3a1dd3c939e466cfd09974418b4431769d9c756a
samy-rosa <sr.oliveira0910@gmail.com>
2021-11-26 16:36:41 -0300

    proj edit capa

--- proj-edit.php
+++ proj-edit.php
@@ -25,7 +25,7 @@
     require_once "includes/function/function-img.php";
     ?>
     <?php
-    
+
     $cod = $_GET['cod'] ?? null;
     if (is_logado()) {
         if (!isset($_POST['nomeProj'])) {
@@ -37,12 +37,15 @@
             $array = $_POST['img'] ?? null;;
             $desc = $_POST['desc'] ?? null;;
             $tag = $_POST['tag'] ?? null;;
-            $sql = "update projeto set proj_name='$nome', proj_desc='$desc', tag_id='$tag' ";
+            $sql = "update projeto set proj_name='$nome', proj_desc='$desc', tag_id='$tag'";
             if (!empty($bg_projet)) {
-                echo "x";
-                $sql .= ", user_foto='proj_back_img'";
+                $ext = pathinfo($_FILES['bg_projet']['name'], PATHINFO_EXTENSION); //Pegando extensão do arquivo
+                $new_name = "YAS-" . date("Y.m.d-H.i.s.") . $ext; //Definindo um novo nome para o arquivo
+                $dir = 'upload/img-proj-bg/'; //Diretório para uploads
+                move_uploaded_file($_FILES['bg_projet']['tmp_name'], $dir . $new_name); //Fazer upload do arquivo
+                $sql .= ", proj_back_img='$new_name'";
             }
-            $sql .=" where proj_id= '$cod'";
+            $sql .= " where proj_id= '$cod'";
             if ($banco->query($sql)) {
                 echo "xx";
             }
